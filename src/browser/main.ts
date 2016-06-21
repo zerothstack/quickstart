87366fd7257ebe121f8fd8aa45f05b4a9826e0f0
Zak Henry <zak.henry@gmail.com>
2016-06-21 23:07:38 +0100

    feat(model): Implement demo for  model creation

--- src/browser/main.ts
+++ src/browser/main.ts
@@ -6,12 +6,14 @@ import { ConsoleLogger, Logger } from '@ubiquits/core/common';
 import { AppComponent } from './app/app.component';
 import { UserStore } from '../common/stores/user.store';
 import { UserHttpStore } from './stores/user.http.store';
+import { UserMockStore } from '../common/stores/user.mock.store';
 
 if (process.env.ENV === 'production') {
   enableProdMode();
 }
 
 bootstrap(AppComponent, [
+  UserMockStore,
   {provide: Logger, useClass: ConsoleLogger},
   {provide: UserStore, useClass: UserHttpStore},
   HTTP_PROVIDERS,
