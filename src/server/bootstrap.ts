d44147a99e4143298e300e081eb434b98993a90a
Zak Henry <zak.henry@gmail.com>
2016-08-03 10:29:40 +0100

    feat(docker): Implement env and docker-compose configuration to handle running with docker locally

--- src/server/bootstrap.ts
+++ src/server/bootstrap.ts
@@ -4,6 +4,10 @@ import bootstrap from './main';
 export default bootstrap().then(({server, logger}) => {
   return server.start().then(() => {
     logger.source('server').info('Server running at:', server.getHost());
+  }).catch((e) => {
+    logger.source('server')
+      .error(e.message)
+      .debug(e);
   });
 
 });
